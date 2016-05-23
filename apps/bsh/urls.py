6c98d584ef5bb68106ff8b88892b91b7f534511a
Erik Asland <erikasland87@gmail.com>
2016-05-22 20:36:50 -0700

    Basically done with mobile part of site. Probably some bugs, but good enough for now. Started on the front page of the desktop.

--- apps/bsh/urls.py
+++ apps/bsh/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
-from views import Landing
+from views import Landing, About, Shop, Contact, Essence, Session
 
 urlpatterns = [
+url(r'^your-session$', Session.as_view()),
+	url(r'^essence$', Essence.as_view()),
+	url(r'^contact$', Contact.as_view()),
+	url(r'^shop$', Shop.as_view()),
+	url(r'^about$', About.as_view()),
     url(r'^$', Landing.as_view()),
 ]
--- apps/bsh/views.py
+++ apps/bsh/views.py
@@ -3,4 +3,24 @@ from django.views.generic import View
 
 class Landing(View):
 	def get(self, request):
-		return render(request, 'bsh/index.html')
+		return render(request, 'bsh/home.html')
+
+class About(View):
+	def get(self, request):
+		return render(request, 'bsh/about.html')
+
+class Shop(View):
+	def get(self, request):
+		return render(request, 'bsh/shop.html')
+
+class Contact(View):
+	def get(self, request):
+		return render(request, 'bsh/contact.html')
+
+class Essence(View):
+	def get(self, request):
+		return render(request, 'bsh/essences.html')
+
+class Session(View):
+	def get(self, request):
+		return render(request, 'bsh/session.html')
