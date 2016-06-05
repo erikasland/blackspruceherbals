04ec8df2ace6a5c1923e2be0363da3ecbe505ba6
Erik Asland <erikasland87@gmail.com>
2016-06-05 16:47:33 -0700

    Hid password from hackers and established basic contact form functionality.

--- blackspruceherbals/settings.py
+++ blackspruceherbals/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'erikasland87@gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_PASSWORD = 'vulpes69'
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
